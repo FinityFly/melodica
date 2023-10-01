0040e295653149f3481ab19d4ee456dffc4f4bc2
Justin Xue <43502772+shoexue@users.noreply.github.com>
2023-09-30 23:17:04 -0400

    3js added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, send_from_directory
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # Configure the uploads folder
 UPLOAD_FOLDER = 'uploads'
